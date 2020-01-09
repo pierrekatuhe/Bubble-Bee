a1e0969a1a0fbb039f4d8d588ebed99c5dd78202
Pierre Katuhe <pierrekatuhe@gmail.com>
2020-01-09 09:25:57 +0800

    Last Update

--- Bubble Bee/My Project/Application.Designer.vb
+++ Bubble Bee/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Bubble_Bee.Login
+            Me.MainForm = Global.Bubble_Bee.MenuCustomer
         End Sub
     End Class
 End Namespace
